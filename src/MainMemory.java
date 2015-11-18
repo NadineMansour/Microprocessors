348da0d02268dcf82ec7bd9cc177f4e379b871d8
mohaazeem <mohaazeem7@gmail.com>
2015-11-18 10:50:29 +0200

    check_Icahce direct_mapped fully set load_program read

--- src/MainMemory.java
+++ src/MainMemory.java
@@ -23,13 +23,28 @@ public class MainMemory {
 		// Hadeel + Mogh + Badr
 		// save the given code in the main memory starting from index "start"
 		// dont forget to handle the case where one line can have more than one instruction
+		int x = start;
+		for(int i = 0;i<program.length;i++) {
+			content[x]=program[i];	
+			x+=2;	
+		}
 	}
 	
 	public String [] read(int index){
 		// Hadeel + Mogh + Badr
 		// we have to read the entire block and not the specific byte in address = index  + the specific instruction and save it
 		// in the last cell in the array
-		return null;
+		String [] read = new String[l+1];
+		int block = index/l;
+		int startb = 0+l*block;
+		int endb = (l-1)+l*block;
+		int j = 0;
+		for(int i = startb;i<=endb;i++) {
+			read[j]=content[i];
+			j++;
+		}
+		read[j]=content[index];
+		return read;
 	}
 	
 	public void write (int index , String block){
